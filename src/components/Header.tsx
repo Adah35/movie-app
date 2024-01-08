66552681096ecbf30dc8e49856804f509c3c1428
adah <joshuaadah6@gmail.com>
2024-01-08 22:03:43 +0100

    changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import GenreDorpdown from './ui/GenreDropdown'
 
 export const Header = () => {
   return (
-    <header className='fixed top-0 z-[1000] w-full flex p-5 items-center justify-between bg-gradient-to-t from-gray-200/0 via-slate-900/25 to-gray-900'>
+    <header className='fixed top-0 z-50 w-full flex p-5 items-center justify-between bg-gradient-to-t from-gray-200/0 via-slate-900/25 to-gray-900'>
       <Link href={'/'} className='mr-10'>
         <Image
           src={'/next.svg'}
